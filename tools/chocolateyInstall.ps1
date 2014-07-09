9ec8a508c2a4223f99d6dc39b986678748eb336b
Romain Lespinasse <romain.lespinasse@gmail.com>
2014-07-10 00:54:26 +0200

    Golo 1.0.0 for chocolatey

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+try {
+  $package = 'golo-1.0.0'
+
+  $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" 
+  ### For BinRoot, use the following instead ###
+  $binRoot = "$env:systemdrive\"
+  ### Using an environment variable to to define the bin root until we implement configuration ###
+  if($env:chocolatey_bin_root -ne $null){$binRoot = join-path $env:systemdrive $env:chocolatey_bin_root}
+  $installDir = Join-Path $binRoot $package
+  Write-Host "Adding `'$installDir`' to the path and the current shell path"
+  $zipUrl = 'http://search.maven.org/remotecontent?filepath=org/golo-lang/golo/1.0.0/golo-1.0.0-distribution.zip'
+
+  Install-ChocolateyZipPackage 'golo' "$zipUrl" "$binRoot"
+  
+  Install-ChocolateyPath "$installDir\bin" 'User' # Machine will assert administrative rights
+
+  Write-ChocolateySuccess 'golo'
+} catch {
+  Write-ChocolateyFailure 'golo' "$($_.Exception.Message)"
+  throw 
+}
---
+++ tools/chocolateyUninstall.ps1
@@ -0,0 +1,14 @@
+try {
+  $package = 'golo-1.0.0'
+  $binRoot = "$env:systemdrive\"
+  ### Using an environment variable to to define the bin root until we implement configuration ###
+  if($env:chocolatey_bin_root -ne $null){$binRoot = join-path $env:systemdrive $env:chocolatey_bin_root}
+  $installDir = Join-Path $binRoot $package
+
+  Remove-Item -Path "$installDir" -Force -Recurse
+
+  Write-ChocolateySuccess 'golo'
+} catch {
+  Write-ChocolateyFailure 'golo' "$($_.Exception.Message)"
+  throw 
+}
