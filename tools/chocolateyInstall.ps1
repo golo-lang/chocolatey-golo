4610a5933d72824efbd7bcb1c9a9093e5a34a950
Romain Lespinasse <romain.lespinasse@gmail.com>
2016-03-02 12:17:02 +0100

    golo 3.1.0-M2

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -5,7 +5,7 @@ try {
   $binRoot = Get-BinRoot
   $installDir = Join-Path $binRoot $package
   Write-Host "Adding `'$installDir`' to the path and the current shell path"
-  $zipUrl = 'https://bintray.com/artifact/download/golo-lang/downloads/golo-3.1.0-incubation-M1.zip'
+  $zipUrl = 'https://bintray.com/artifact/download/golo-lang/downloads/golo-3.1.0-incubation-M2.zip'
 
   Install-ChocolateyZipPackage 'golo' "$zipUrl" "$binRoot"
 
