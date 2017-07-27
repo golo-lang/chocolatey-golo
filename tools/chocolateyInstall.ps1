e1122e0b36347350c8a33c30b4f72e10a09cd768
Romain Lespinasse <romain.lespinasse@gmail.com>
2017-07-27 14:15:12 +0200

    add checksum

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,5 +1,6 @@
 try {
   $package = 'golo'
+  $checksum = 'BF61B5A2565C31ED39829453D2C129C4604EFAAC6F6A7E546D199EC82E9EC654'
 
   $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   $binRoot = Get-BinRoot
@@ -7,8 +8,13 @@ try {
   Write-Host "Adding `'$installDir`' to the path and the current shell path"
   $zipUrl = 'https://bintray.com/artifact/download/golo-lang/downloads/golo-3.2.0.zip'
 
-  Install-ChocolateyZipPackage 'golo' "$zipUrl" "$binRoot"
-
+  Install-ChocolateyZipPackage `
+      -PackageName $package `
+      -Url $zipUrl `
+      -UnzipLocation $binRoot `
+      -Checksum $checksum `
+	    -ChecksumType 'sha256'
+      
   Install-ChocolateyPath "$installDir\bin" 'User' # Machine will assert administrative rights
 } catch {
   throw $_.Exception
